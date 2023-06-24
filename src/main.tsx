92c63b801890d0d771182c49a7ee673e4a17904b
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-06-23 22:42:42 -0400

    Configure web3 libs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import { WagmiConfig } from 'wagmi'
 
 import App from './App.tsx'
+import { chains, wagmiConfig } from './providers.ts'
 import './styles/normalize.css'
 import './styles/style.css'
 
@@ -11,7 +15,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={lightTheme}>
       <ThorinGlobalStyles />
-      <App />
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains} modalSize="compact">
+          <App />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ThemeProvider>
   </React.StrictMode>
 )
