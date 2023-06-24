c9cee8e6d75657eff3be775a05d02d8f99c8c20b
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-06-23 22:20:40 -0400

    Remove boiler plate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
+import './styles/normalize.css'
+import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={lightTheme}>
+      <ThorinGlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
