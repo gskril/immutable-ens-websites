010e7f348d1afb2eaec76644942c68340335f60e
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-06-25 02:06:09 -0400

    Lock contenthash

--- src/components/screens/LockContentHash.tsx
+++ src/components/screens/LockContentHash.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@ensdomains/thorin'
+import { namehash } from 'viem/ens'
+import {
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from 'wagmi'
+
+import { getResolverContract } from '../../contracts'
 import { Card, CardDescription, Container } from '../atoms'
 
 type Props = {
@@ -7,6 +17,18 @@ type Props = {
 }
 
 export function LockContentHash({ name, subname, nextStep }: Props) {
+  const { chain } = useNetwork()
+  const resolver = getResolverContract(chain?.id)
+
+  const prepareTx = usePrepareContractWrite({
+    ...resolver,
+    functionName: 'lockContenthash',
+    args: [namehash(`${subname}.${name}.eth`)],
+  })
+
+  const tx = useContractWrite(prepareTx.config)
+  const receipt = useWaitForTransaction({ hash: tx.data?.hash })
+
   return (
     <Container>
       <Card title="Lock IPFS Website">
@@ -18,6 +40,26 @@ export function LockContentHash({ name, subname, nextStep }: Props) {
           </strong>
           .
         </CardDescription>
+
+        {receipt.isSuccess ? (
+          <Button onClick={() => nextStep()}>Continue</Button>
+        ) : receipt.isError ? (
+          <Button disabled>Transaction Failed</Button>
+        ) : receipt.isLoading ? (
+          <Button disabled loading>
+            Transaction Processing
+          </Button>
+        ) : tx.isLoading ? (
+          <Button disabled loading>
+            Confirm in Wallet
+          </Button>
+        ) : prepareTx.isError ? (
+          <Button disabled>Error Preparing Tx</Button>
+        ) : (
+          <Button disabled={!tx.write} onClick={() => tx.write?.()}>
+            Save
+          </Button>
+        )}
       </Card>
     </Container>
   )
